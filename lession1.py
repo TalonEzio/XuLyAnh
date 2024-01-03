74857dd9581ed4b1790698b8202e3ee4bd262e63
TalonEzio <vuthemanh1707@gmail.com>
2024-01-03 13:13:28 +0700

    Xu ly anh init

---
+++ lession1.py
@@ -0,0 +1,40 @@
+import random
+def KhoiTao(seed = 10):
+    global a, b, c
+    a = random.randint(0, seed)
+    b = random.randint(0, seed)
+    c = random.randint(0, seed)
+
+def HienThi():
+    print('a = ', a)
+    print('b = ', b)
+    print('c = ', c)
+
+def Tong(): return a + b + c
+
+
+def MaxMin():
+    min_var = min(a, b, c)
+    max_var = max(a, b, c)
+
+    print('max = ', max_var)
+    print('min = ', min_var)
+
+
+def TimKiem(x):
+    exist = False
+    if x == a or x == b or x == c:
+        exist = True
+    if exist:
+        print(f'co ton tai {x}')
+    else:
+        print(f'khong ton tai {x}')
+
+
+if __name__ == '__main__':
+    KhoiTao()
+    HienThi()
+    MaxMin()
+    print(f'tong = {Tong()}')
+    x = int(input('gia tri can tim: x = '))
+    TimKiem(x)
